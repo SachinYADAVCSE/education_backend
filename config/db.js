ade86fab77f793ece8c57e8e0e78fada2f9e5d60
SachinYadavCSE <sachinyadav53730@gmail.com>
2025-09-08 00:38:53 +0530

    Added MongoDb URI as well

--- config/db.js
+++ config/db.js
@@ -1,12 +1,21 @@
 import mongoose from 'mongoose';
+import { MongoClient } from 'mongodb';
 
 // just install the driver
 // Add the MONGODB_URI here
 const MONGODB_URI = process.env.MONGO_URI
+
+const client = new MongoClient(MONGODB_URI);
+
+
 console.log("This coming from db.js, for URI", MONGODB_URI);
 
 const dbConnect = async () => {
     try{
+        const result_client = await client.connect();
+        const db = client.db("languageWeb");
+        const userCollection = db.collection("users");
+        
         const result = await mongoose.connect(MONGODB_URI);
         if(result){
             console.log(`✅ Database Connected: ${result.connection.host}`);
